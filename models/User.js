92a91cf4f23df2519242f6c9f8eef0e50651e384
classuser134 <classuser134@local.iful.edu.ua>
2026-03-11 11:06:29 +0200

    Add Sequelize setup, User model, config folder and update app.js

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  underscored: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+module.exports = User;
